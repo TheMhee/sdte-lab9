3ed49fb6d4972cd0d093c190a247446f37d3c5e0
TheMhee <thanakrit.mh@gmail.com>
2021-04-01 17:04:03 +0000

    step 12

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # terraform.tf
-=======
-#terraform.tf
->>>>>>> 2871848ebeb46bdf7fb845aa91a2e9ae35ec10d9
 terraform {
  backend "s3" {
     encrypt = true
@@ -10,8 +6,4 @@ terraform {
     region = "us-east-1"
     key = "infra-state"
  }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2871848ebeb46bdf7fb845aa91a2e9ae35ec10d9
